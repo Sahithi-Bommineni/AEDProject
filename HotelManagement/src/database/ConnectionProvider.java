8f7a514a3460dfb42869d2e622e8b1d6a0f17ebd
nikhithajarabana <jarabana.n@northeastern.edu>
2022-12-09 18:35:16 -0500

    nikhitha commit

--- HotelManagement/src/database/ConnectionProvider.java
+++ HotelManagement/src/database/ConnectionProvider.java
@@ -29,5 +29,29 @@ public class ConnectionProvider {
             JOptionPane.showMessageDialog(null,e);
         }
         return null;
+        /*try{
+            JSONParser parser = new JSONParser();
+            Object obj = new JSONParser().parse(new FileReader("./src/database/dbConfig.json"));
+            JSONObject jObj = (JSONObject) obj;
+            JSONObject jObj2 = (JSONObject) jObj.get("sqlConnection");
+            System.out.print(jObj2.toJSONString());
+            String url = jObj2.get("url").toString();
+            String username = jObj2.get("username").toString();
+            String password = jObj2.get("password").toString();
+            System.out.print("2");
+            System.out.println(url+","+username+","+password);
+            
+            Connection con = DriverManager.getConnection(url,username,password);
+            System.out.print("Connection Successful");
+            
+            Statement st = con.createStatement();
+            String query = "USE hotel_aed";
+            st.executeUpdate(query);
+            
+            return con;
+        }catch(IOException | SQLException | ParseException e){
+            System.out.print(e.getMessage());
+            return null;
+        }*/
     }
 }
