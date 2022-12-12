3391198c34f6e4d0c5cd949c8fdb16ed7ae76ef5
koushalamshala <koushalamshala@localhost>
2022-12-11 22:45:12 -0500

    UI editing

--- HotelManagement/src/ui/CustomerDetailsBill.java
+++ HotelManagement/src/ui/CustomerDetailsBill.java
@@ -73,10 +73,10 @@ public class CustomerDetailsBill extends javax.swing.JFrame {
         setPreferredSize(new java.awt.Dimension(1300, 577));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        CustomerDetailsBillLbl.setFont(new java.awt.Font("Helvetica Neue", 1, 13)); // NOI18N
+        CustomerDetailsBillLbl.setFont(new java.awt.Font("Helvetica Neue", 1, 24)); // NOI18N
         CustomerDetailsBillLbl.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         CustomerDetailsBillLbl.setText("CUSTOMER DETAILS BILL");
-        getContentPane().add(CustomerDetailsBillLbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 19, 180, 31));
+        getContentPane().add(CustomerDetailsBillLbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 440, 31));
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -100,6 +100,7 @@ public class CustomerDetailsBill extends javax.swing.JFrame {
         jLabel1.setText("CLICK ON TABLE ROW TO OPEN BILL");
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 490, 318, 27));
 
+        BackBtn.setBackground(new java.awt.Color(153, 204, 255));
         BackBtn.setFont(new java.awt.Font("Helvetica Neue", 1, 13)); // NOI18N
         BackBtn.setText("Back");
         getContentPane().add(BackBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(910, 19, -1, 31));
